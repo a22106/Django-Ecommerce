98abae73f4d8e3ec3ff5e64d183cb0cf1c59d811
PiusHwang <bk22106@gmail.com>
2024-09-18 16:20:45 +0900

    [feat] to detail product page when it click the picture

--- core/urls.py
+++ core/urls.py
@@ -18,10 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-import shop
 from shop import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name="index"),
+    path("detail/<int:id>", views.detail, name="detail"),
 ]
